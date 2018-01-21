2394dad4124480cc0c4f5091e2c2753660144080
Luke <LukeReeder95@gmail.com>
2018-01-21 21:39:56 +0000

    Finished 12 and 13

--- labs/practicals/13_Rotating_Triangle/13_Rotating_Triangle.cpp
+++ labs/practicals/13_Rotating_Triangle/13_Rotating_Triangle.cpp
@@ -45,7 +45,7 @@ bool update(float delta_time) {
 bool render() {
   // Bind effect
   renderer::bind(eff);
-  mat4 R;
+  mat4 R = rotate(mat4(1.0f), (3.1415f / 2), vec3(1.0f, 0.0f, 0.0f));
   // *********************************
   // Create rotation matrix - rotate around Z axis by theta
 
